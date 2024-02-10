96ed1f9abe8dd65357afc224a04963d9a6758eef
alithebsst12 <126507333+alithebsst12@users.noreply.github.com>
2024-02-10 00:43:09 -0500

    new code Test

--- tests/Americanexpress.spec.ts
+++ tests/Americanexpress.spec.ts
@@ -1,11 +1,20 @@
 import { test, expect, type Page } from '@playwright/test';
 test('navigatetogtawebsite', async ({ page }) => {
 
+    
+   let results = ["men"]
+
+   results.push('women')
+
+   results.push('kids')
+
+
+   for(let i = 0; i< results.length; i++){ 
 
 
     //go to american website
     await page.goto('https://card.americanexpress.com/d/american-express/')
-    // wait 6 seconc
+    // wait 6 seconcd
     await page.waitForTimeout(6000)
     // now verify url 
     await expect(page).toHaveURL('https://card.americanexpress.com/d/american-express/')
@@ -13,19 +22,48 @@ test('navigatetogtawebsite', async ({ page }) => {
     await expect(page).toHaveTitle('American Express Cards')
     // now click on platnum card
     await page.locator('xpath=//*[@class ="max-w-[170px] text-p2 text-left ml-12 border-solid border-b-4 border-amex-brightBlue"]').click({timeout:3000})
-     // wait 6 seconc
+     // wait 6 seconcd
      await page.waitForTimeout(6000)
      // now scroll by pixels
      await page.mouse.wheel(0,600)
-     // wait 6 seconc
-     await page.waitForTimeout(6000)
+     // wait 3 seconcd
+     await page.waitForTimeout(3000)
      // now campture information
      let myText = await page.locator('xpath =(//*[@class = "m-auto max-w-content"])[3]').textContent()
      // print it
      console.log(myText)
-
+     // now click first card
+     await page.locator('xpath=(//*[@class = "grid grid-flow-col justify-center items-center gap-8 font-bold text-t5 mb-20"])[1]').click({timeout:3000})
+     // wait 2 seconcd
+     await page.waitForTimeout(2000)
+     // now click second card
+     await page.locator('xpath=(//*[@class = "grid grid-flow-col justify-center items-center gap-8 font-bold text-t5 mb-20"])[2]').click({timeout:3000})
+     // wait 2 seconcd
+     await page.waitForTimeout(2000)
+     // now click on compare 
+     await page.locator('xpath=//*[text()="Compare 2 Selected Cards"]').click({timeout:3000})
+     // wait 5 seconcd
+     await page.waitForTimeout(5000)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    }// end of loop statment 
    
-
+    
 
 
 
