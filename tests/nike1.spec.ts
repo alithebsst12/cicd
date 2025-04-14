941fb439d929736a1bbd862d80ec90a96f142b63
Ali <darkprinceali02@gmail.com>
2025-04-14 16:33:41 -0400

    hi

---
+++ tests/nike1.spec.ts
@@ -0,0 +1,39 @@
+
+import { test, expect, type Page } from '@playwright/test';
+test('navigatetolvwebsite', async ({ page }) => {
+
+
+    await page.goto("https://www.nike,com");
+    await page.waitForTimeout(3000)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
